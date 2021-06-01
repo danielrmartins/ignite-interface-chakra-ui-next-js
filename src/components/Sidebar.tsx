780cd3a642d8369b53a4a15b8992a1d23dadb9dd
Daniel Martins <danielmartins10@gmail.com>
2021-06-01 15:14:26 -0300

    lista de usuários

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -18,6 +18,19 @@ export function Sidebar() {
             </Link>
           </Stack>
         </Box>
+        <Box>
+          <Text fontWeight="bold" color="gray.400" fontSize="small">AUTOMAÇÃO</Text>
+          <Stack spacing="4" mt="8" align="stretch">
+            <Link display="flex" align="center">
+              <Icon as={RiDashboardLine} fontSize="20"/>
+              <Text ml="4" fontWeight="medium">Dashboard</Text>
+            </Link>
+            <Link display="flex" align="center">
+              <Icon as={RiContactsLine} fontSize="20"/>
+              <Text ml="4" fontWeight="medium">Usuários</Text>
+            </Link>
+          </Stack>
+        </Box>
       </Stack>
     </Box>
   )
