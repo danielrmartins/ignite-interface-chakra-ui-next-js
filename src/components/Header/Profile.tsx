8df7f6547fd672fb7fcedb3bc00a26128afa8f08
Daniel Martins <danielmartins10@gmail.com>
2021-06-02 14:14:22 -0300

    header responsivo

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,13 +1,20 @@
 import { Avatar } from '@chakra-ui/avatar';
 import { Box, Flex, Text } from '@chakra-ui/layout';
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Daniel Martins</Text>
-        <Text color="gray.300" fontSize="small">daniel@gmail.com</Text>
-      </Box>
+      {showProfileData &&
+        <Box mr="4" textAlign="right">
+          <Text>Daniel Martins</Text>
+          <Text color="gray.300" fontSize="small">daniel@gmail.com</Text>
+        </Box>
+      }
+
 
       <Avatar size="md" name="Daniel Martins" src="https://github.com/danielrmartins.png" />
     </Flex>
