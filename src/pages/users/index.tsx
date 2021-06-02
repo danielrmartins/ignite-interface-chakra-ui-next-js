6b077f6684fd517d62b02e019c3ffa5728d73e24
Daniel Martins <danielmartins10@gmail.com>
2021-06-02 15:24:52 -0300

    fluxo de navegação

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,4 +1,5 @@
 import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from '@chakra-ui/react';
+import Link from 'next/link';
 import { RiAddLine, RiPencilLine } from 'react-icons/ri';
 import { Header } from '../../components/Header';
 import { Pagination } from '../../components/Pagination';
@@ -17,7 +18,9 @@ export default function UserList() {
         <Box flex="1" borderRadius={8} bg="gray.800" p="8">
           <Flex mb="8" justify="space-between" align="center">
             <Heading size="lg" fontWeight="normal">Usuários</Heading>
-            <Button as="a" size="sm" fontSize="sm" colorScheme="pink" leftIcon={<Icon as={RiAddLine} fontSize="20" />}>Criar Novo</Button>
+            <Link href="/users/create" passHref>
+              <Button as="a" size="sm" fontSize="sm" colorScheme="pink" leftIcon={<Icon as={RiAddLine} fontSize="20" />}>Criar Novo</Button>
+            </Link>
           </Flex>
 
           <Table colorScheme="whiteAlpha">
@@ -29,7 +32,7 @@ export default function UserList() {
                 <Th>Usuário</Th>
                 {isWideVersion && (
                   <>
-                    <Th>Data de cadastro</Th>      
+                    <Th>Data de cadastro</Th>
                     <Th width="8" />
                   </>
                 )}
