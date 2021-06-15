1683004b130f4dbc760cf403333936668b429227
Daniel Martins <danielmartins10@gmail.com>
2021-06-15 12:00:30 -0300

    configurando axios e sinalizando refatch

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -5,11 +5,11 @@ import { RiAddLine, RiPencilLine } from 'react-icons/ri';
 import { Header } from '../../components/Header';
 import { Pagination } from '../../components/Pagination';
 import { Sidebar } from '../../components/Sidebar';
+import { api } from '../../services/api';
 
 export default function UserList() {
-  const { data, isLoading, error } = useQuery('users', async () => {
-    const response = await fetch('http://localhost:3000/api/users');
-    const data = await response.json();
+  const { data, isLoading, isFetching, error } = useQuery('users', async () => {
+    const { data } = await api.get('users');
 
     const users = data.users.map(user => {
       return {
@@ -38,7 +38,7 @@ export default function UserList() {
         <Sidebar />
         <Box flex="1" borderRadius={8} bg="gray.800" p="8">
           <Flex mb="8" justify="space-between" align="center">
-            <Heading size="lg" fontWeight="normal">Usuários</Heading>
+            <Heading size="lg" fontWeight="normal">Usuários {!isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4" />}</Heading>
             <Link href="/users/create" passHref>
               <Button as="a" size="sm" fontSize="sm" colorScheme="pink" leftIcon={<Icon as={RiAddLine} fontSize="20" />}>Criar Novo</Button>
             </Link>
